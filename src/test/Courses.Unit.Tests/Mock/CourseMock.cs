3f10fdfe57e2e5df8d01fda3734a9c37294d0de7
Andre Costa <andrecosta.mirante@wizsolucoes.com.br>
2021-02-28 18:52:46 -0300

    testes

--- src/test/Courses.Unit.Tests/Mock/CourseMock.cs
+++ src/test/Courses.Unit.Tests/Mock/CourseMock.cs
@@ -1,4 +1,5 @@
 ﻿using Bogus;
+using Courses.Application.ViewModels;
 using Courses.Domain.Models.Course;
 using System;
 
@@ -9,6 +10,19 @@ namespace Courses.Unit.Tests.Mock
         public static Faker<Course> CourseModelFaker =>
             new Faker<Course>()
             .CustomInstantiator(x => new Course { 
+                Id = x.Random.Int(),
+                CategoryId = x.Random.Int(),
+                Name = x.Finance.AccountName(),
+                Description = x.Database.Collation(),
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddYears(1),
+                StudentsPerClass = x.Random.Int()
+            });
+
+        public static Faker<CourseViewModel> CourseViewModelFaker =>
+            new Faker<CourseViewModel>()
+            .CustomInstantiator(x => new CourseViewModel
+            {
                 Id = x.Random.Int(),
                 CategoryId = x.Random.Int(),
                 Name = x.Finance.AccountName(),
